8d42678d5fec4e2aadd0e0328044efdc685a30be
shievaniR <68860438+shievaniR@users.noreply.github.com>
2020-10-31 14:14:59 +0530

    Add files via upload

--- quotes.js
+++ quotes.js
@@ -5,7 +5,7 @@ const Quotes = ()=>{
     useEffect(() =>{
         getquote()
     },[])
-}
+
 const getquote = () =>{
     let url = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
     fetch(url)
@@ -45,3 +45,4 @@ return(
 
         )
 }
+export default Quotes;
