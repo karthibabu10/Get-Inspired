7061984ffc24d9465d2d9786612039edbc1ccdb1
rakshayac <60009775+rakshayac@users.noreply.github.com>
2020-10-31 13:52:03 +0530

    Add files via upload

--- quotes.js
+++ quotes.js
@@ -8,4 +8,19 @@ const Quotes = ()=>{
 }
 const getquote = () =>{
     let url = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>{
+        let dataQuotes = data.quotes;
+        let randomNum = Math.floor(Math.random() * dataQuotes.length);
+        let randomQuotes = dataQuotes[randomNum];
+        setquote(randomQuotes.quote);
+        setauthor(randomQuotes.author);
+
+
+    })
+
+
+
+
 }
